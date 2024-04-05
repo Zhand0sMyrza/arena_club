d90ce8779d007aec971e92bc42aef49e459ea74b
Zhandos Agzamov <stickysplay@gmail.com>
2024-04-05 13:42:16 +0500

    add ios/android native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:arena_club/app_main/app_main.dart';
 import 'package:arena_club/app_main/bloc/app_main_bloc.dart';
 import 'package:arena_club/data/repository/repository.dart';
-import 'package:arena_club/firebase_options.dart';
 import 'package:arena_club/screens/authorization_screen/src/bloc/auth_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,7 +11,6 @@ Future<void> main() async {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     RepositoryProvider(
       create: (context) => Repository(),
